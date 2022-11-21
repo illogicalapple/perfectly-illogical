497f581c154a61f9662cf18fdca80b5d92f74791
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-11-21 08:59:34 -0600

    add smol thing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+const $ = (...args) => document.querySelector(...args);
 
+async function start() {
+  var thingy = await fetch("/id.json").then(r => r.json());
+  var names = thingy.filenames;
+  $("img.comic").src = `/${names[names.length - 1]}`;
+}
+
+window.addEventListener("load", function() {
+  start();
+});
